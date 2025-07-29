95c60a9dc0db02c134069387e88e813f3af363c1
Yiqun Liu <Xreki@users.noreply.github.com>
2025-07-29 16:59:36 +0800

    Add ci to check validation for modified models. (#37)
    * Change to use public resource of github.
    
    * Apply pre-commit for samples.
    
    * Add checking validation script for ci testing.
    
    * Add workflow yml for validate.
    
    * Add clone and checkout step.
    
    * Add install of torch.

---
+++ tools/ci/check_validate.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set +ex
+
+function LOG {
+  echo [$0:${BASH_LINENO[0]}] $* >&2
+}
+
+LOG "[INFO] Start validate samples changed by pull request ..."
+
+export GRAPH_NET_EXTRACT_WORKSPACE=$(cd $(dirname $0)/../.. && pwd)
+export PYTHONPATH=${GRAPH_NET_EXTRACT_WORKSPACE}:$PYTHONPATH
+
+[ -z "$CUDA_VISIBLE_DEVICES" ] && CUDA_VISIBLE_DEVICES="0"
+
+function prepare_env() {
+  num_changed_files=$(git diff --name-only develop | grep -E "samples/(.*\.py|.*\.json)" | wc -l)
+  if [ ${num_changed_files} -eq 0 ]; then
+    LOG "[INFO] This pull request doesn't change any files of op benchmark, skip the CI."
+    exit 0
+  fi
+
+  LOG "[INFO] Device Id: ${CUDA_VISIBLE_DEVICES}"
+  # Update pip
+  LOG "[INFO] Update pip ..."
+  env http_proxy="" https_proxy="" pip install -U pip > /dev/null
+  [ $? -ne 0 ] && LOG "[FATAL] Update pip failed!" && exit -1
+  # install torch 
+  pip install torch==2.7.0 --index-url https://download.pytorch.org/whl/cu118
+}
+
+function check_validation() {
+  LOG "[INFO] Start run validate for changed samples ..."
+  MODIFIED_MODEL_PATHS=()
+  for file in $(git diff --name-only develop | grep -E "samples/(.*\.py|.*\.json)")
+  do
+    LOG "[INFO] Found ${file} modified."
+    model_path=$(dirname ${file})
+    MODIFIED_MODEL_PATHS[${#MODIFIED_MODEL_PATHS[@]}]=$model_path
+  done
+  MODIFIED_MODEL_PATHS=($(echo ${MODIFIED_MODEL_PATHS[@]} | tr ' ' '\n' | sort | uniq))
+  LOG "[INFO] Validation of these models will run: ${MODIFIED_MODEL_PATHS[@]}"
+  fail_name=()
+  for model_path in ${MODIFIED_MODEL_PATHS[@]}
+  do
+    python -m graph_net.torch.validate --model-path ${GRAPH_NET_EXTRACT_WORKSPACE}/${model_path} >&2
+    [ $? -ne 0 ] && fail_name[${#fail_name[@]}]="${model_path}(Run on ${device_type})"
+  done
+  if [ ${#fail_name[@]} -ne 0 ]
+  then
+    LOG "[FATAL] Failed tests: ${fail_name[@]}"
+    echo ${fail_name[@]}
+    exit -1
+  fi
+}
+
+function summary_problems() {
+  local check_validation_code=$1
+  local check_validation_info=$2
+  if [ $check_validation_code -ne 0 ]
+  then
+    LOG "[FATAL] ============================================"
+    LOG "[FATAL] Summary problems:"
+    LOG "[FATAL] === API test error - Please fix the failed API tests accroding to fatal log:"
+    LOG "[FATAL] $check_validation_info"
+    exit $check_validation_code
+  fi
+}
+
+function main() {
+  prepare_env
+  check_validation_info=$(check_validation)
+  check_validation_code=$?
+  summary_problems $check_validation_code "$check_validation_info"
+  LOG "[INFO] check_validation run success and no error!"
+}
+
+main
