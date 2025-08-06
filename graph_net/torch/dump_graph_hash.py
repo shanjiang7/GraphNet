7c02140466a97402590e6848b0755297b273358d
Li Xinqi <lixinqi2010@gmail.com>
2025-08-06 13:39:14 +0800

    reset graph_hash.txt (#106)
    * support checking model redundancy
    
    * revert change of vision_model_test
    
    * reformat python code.
    
    * reformat bert_model_test.py and utils.py
    
    * minor fix
    
    * fix failed check by comparing directories after os.path.realpath()
    
    * fix bugs in check_validate.sh
    
    * set dynamic=False in single_device_runner.py
    
    * reset graph hash

---
+++ graph_net/torch/dump_graph_hash.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+import tempfile
+import sys
+import contextlib
+import graph_net
+
+
+@contextlib.contextmanager
+def temp_workspace():
+    with tempfile.TemporaryDirectory() as tmp_dir_name:
+        old = os.environ.get("GRAPH_NET_EXTRACT_WORKSPACE")
+        os.environ["GRAPH_NET_EXTRACT_WORKSPACE"] = tmp_dir_name
+        yield tmp_dir_name
+        os.environ["GRAPH_NET_EXTRACT_WORKSPACE"] = old
+
+
+def main(args):
+    model_path = args.model_path
+    with temp_workspace() as tmp_dir_name:
+        print("dump-graph-hash ...")
+        extract_name = "temp"
+        cmd = f"{sys.executable} -m graph_net.torch.single_device_runner --model-path {model_path} --enable-extract True --extract-name {extract_name} --dump-graph-hash-key"
+        cmd_ret = os.system(cmd)
+        assert cmd_ret == 0, f"{cmd_ret=}, {cmd=}"
+
+        print(f"dump-graph-hash success, {model_path=}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="load and run model")
+    parser.add_argument(
+        "--model-path",
+        type=str,
+        required=True,
+        help="Path to folder e.g '../../samples/torch/resnet18'",
+    )
+    args = parser.parse_args()
+    main(args=args)
