b1fc72877930d9418b254e4ba6e401d58ba88a8c
Yiqun Liu <Xreki@users.noreply.github.com>
2025-08-11 10:02:08 +0800

    Fix the path of samples in count graphnet. (#157)

--- tools/count_sample.py
+++ tools/count_sample.py
@@ -1,8 +1,13 @@
 # 统计 samples 目录下， graph_net.json 文件的数量
 import os
 
+
+filename = os.path.abspath(__file__)
+root_dir = os.path.dirname(os.path.dirname(filename))
+samples_dir = os.path.join(root_dir, "samples")
+
 graph_net_count = 0
-for root, dirs, files in os.walk("../samples"):
+for root, dirs, files in os.walk(samples_dir):
     for file in files:
         if file == "graph_net.json":
             graph_net_count += 1
