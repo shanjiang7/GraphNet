cfd47b6c4f80a2ef896b34bcf3e8c45a883bb913
Yiqun Liu <Xreki@users.noreply.github.com>
2025-08-12 14:30:50 +0800

    Add detail of sample statistics for different model category and call it in ci. (#162)

--- tools/count_sample.py
+++ tools/count_sample.py
@@ -5,10 +5,20 @@ import os
 filename = os.path.abspath(__file__)
 root_dir = os.path.dirname(os.path.dirname(filename))
 samples_dir = os.path.join(root_dir, "samples")
+model_categories = os.listdir(samples_dir)
 
 graph_net_count = 0
-for root, dirs, files in os.walk(samples_dir):
-    for file in files:
-        if file == "graph_net.json":
-            graph_net_count += 1
+graph_net_dict = {}
+for category in model_categories:
+    category_dir = os.path.join(samples_dir, category)
+    if os.path.isdir(category_dir):
+        graph_net_dict[category] = 0
+        for root, dirs, files in os.walk(category_dir):
+            if "graph_net.json" in files:
+                graph_net_count += 1
+                graph_net_dict[category] += 1
+
 print(f"Number of graph_net.json files: {graph_net_count}")
+for name, number in graph_net_dict.items():
+    print(f"- {name:24}: {number}")
+print()
