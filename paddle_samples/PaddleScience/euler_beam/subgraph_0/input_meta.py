94f7ecedf5e4ab2e8ead3ac087a280828e0d095e
Wenhao.Dai <wenhao.dai633@gmail.com>
2025-09-08 16:09:30 +0800

    [New Sample] Add test example of PaddleScience (#261)
    * [New Sample] Add test example of PaddleScience
    
    * [New Sample] Add test example of PaddleScience
    
    * [New Sample] Add test example of PaddleScience
    
    * [New Sample] Add test example of PaddleScience

---
+++ paddle_samples/PaddleScience/euler_beam/subgraph_0/input_meta.py
@@ -0,0 +1,34 @@
+class Program_weight_tensor_data_0:
+    name = "data_0"
+    shape = [100, 1]
+    dtype = "float32"
+    min_val = float("0")
+    max_val = float("0.5")
+    data = None
+
+
+class Program_weight_tensor_data_1:
+    name = "data_1"
+    shape = [100, 1]
+    dtype = "float32"
+    min_val = float("0")
+    max_val = float("0.5")
+    data = None
+
+
+class Program_weight_tensor_data_2:
+    name = "data_2"
+    shape = [100, 1]
+    dtype = "float32"
+    min_val = float("0")
+    max_val = float("0.5")
+    data = None
+
+
+class Program_weight_tensor_data_3:
+    name = "data_3"
+    shape = [100, 1]
+    dtype = "float32"
+    min_val = float("0")
+    max_val = float("0.5")
+    data = None
