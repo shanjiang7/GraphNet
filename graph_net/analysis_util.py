Fix analysis calculation logic (#530)
* Fix analysis

* convert

* remove eager_fail, reference_fail from never tolerance

graph_net/analysis_util.py
@@ -4,7 +4,8 @@ import sys
 from scipy.stats import gmean
 from graph_net.config.datatype_tolerance_config import get_precision
 from graph_net.positive_tolerance_interpretation import PositiveToleranceInterpretation
-from graph_net.verify_aggregated_params import determine_tolerances
+
+from graph_net.samples_statistics import determine_tolerances
 from graph_net.positive_tolerance_interpretation_manager import (
     get_positive_tolerance_interpretation,
 )